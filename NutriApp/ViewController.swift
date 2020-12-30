7e618bd2e05337fc69750c8213998fcf3104a0bc
Fahim Jatmiko <fahim.jatmiko@gmail.com>
2020-12-30 11:07:39 +0700

    Initial Commit

---
+++ NutriApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NutriApp
+//
+//  Created by Fahim Jatmiko on 30/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
